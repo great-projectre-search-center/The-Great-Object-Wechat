7db8dcaa48142b196517ea05004acfd18d81aeee
libin257217 <254074270@qq.com>
2020-04-24 09:41:34 +0800

    新增两点位置计算bug去除

--- pages/findway/findway.js
+++ pages/findway/findway.js
@@ -3,66 +3,60 @@
 
 var QQMapWX = require('../../libs/qqmap-wx-jssdk.js');
 var qqmapsdk;
+
 Page({
+
+  data:{
+    add1 : "",
+    add2 : ""
+  },
 
   onLoad: function () {
     qqmapsdk = new QQMapWX({
       key: 'LQXBZ-ZSFWG-AGDQ3-IXFEB-ULEOV-QHBH7'
     });
-
     
   },
 
-  //此函数计算两点之间距离
-  //参数：两个有效地址（不能没有区县：北京市海淀区彩和坊路海淀西大街74号）
-  //思路：地址解析->距离计算
+  //计算两点之间距离
   formSubmit(e) {
-    var _this = this;
-    var add1;
-    var add2;
-    //1.地址解析
+
+    var add1 = "";
+    var add2 = "";
+
+    //地址解析add1
     qqmapsdk.geocoder({
       address: e.detail.value.geocoder, //地址参数
-      success: function (res) {//成功后的回调
-        add1 = res.result.location
-        console.log(add1);
+      success: function (res) {
+        add1 = res.result.location.lat + "," + res.result.location.lng
+        console.log("add1位置:"+add1);
+        //地址解析add2
+        qqmapsdk.geocoder({
+          address: e.detail.value.geocoder1, //地址参数
+          success: function (res) {
+            add2 = res.result.location.lat + "," + res.result.location.lng
+            console.log("add2位置:" +add2);
+            //调用接口计算距离
+            qqmapsdk.calculateDistance({
+              from: String(add1), 
+              to: String(add2), 
+              success: function (res) {//成功后的回调
+                console.log("距离"+res.result.elements[0].distance);
+              },
+              fail: function (error) {
+                console.error(error);
+              }
+            });
+          },
+          fail: function (error) {
+            console.error(error);
+          }
+        })
       },
       fail: function (error) {
         console.error(error);
       }
     })
-    qqmapsdk.geocoder({
-      address: e.detail.value.geocoder1, //地址参数
-      success: function (res) {//成功后的回调
-        add2 = res.result.location
-        console.log(add2);
-      },
-      fail: function (error) {
-        console.error(error);
-      }
-    })
-    //调用距离计算接口
-    qqmapsdk.calculateDistance({
-      //mode: 'driving',//可选值：'driving'（驾车）、'walking'（步行），不填默认：'walking',可不填
-      //from参数不填默认当前地址
-      //获取表单提交的经纬度并设置from和to参数（示例为string格式）
-      from: [{
-        latitude: add1.lat,
-        longitude: add2.lng
-      }] || '', //若起点有数据则采用起点坐标，若为空默认当前地址
-      to: [{
-        latitude: add2.lat,
-        longitude: add2.lng
-      }], //终点坐标
-      success: function (res) {//成功后的回调
-        console.log(res);
-      },
-      fail: function (error) {
-        console.error(error);
-      },
-      complete: function (res) {
-        console.log("错误");
-      }
-    });
   }
+  
 })
