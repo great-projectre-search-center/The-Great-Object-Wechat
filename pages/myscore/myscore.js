7bcaefc9a7582ea9a2c15ecb64a0c774f444c76b
ZZZXXXCCCWXY999 <zzzxxxcccwxy999@gmail.com>
2020-06-02 17:00:52 +0800

    积分管理写了一半

--- pages/myscore/myscore.js
+++ pages/myscore/myscore.js
@@ -8,8 +8,81 @@ Page({
    * 页面的初始数据
    */
   data: {
-    soc:1014  //获取当前积分
+    soc:"",  //获取当前积分
+    details:"",
+    openid:"",
+    baseurl:"",
 
+  },
+  getlast:function(){
+    var that=this
+    wx.request({
+      url: app.globalData.baseurl+'/reward/'+app.globalData.openid+'/getlast',
+      method:'GET',
+      header:{
+        Authorization:wx.getStorageSync('token')
+      },
+      success:function(res){
+          that.setData({
+            soc:res.data.reward
+          })
+      }
+    })
+  },
+  geshihua:function(data){
+    alert(data)
+      str=data.toString()
+      alert(str)
+      sstr=str.split("-")
+      alert(sstr)
+      ans=sstr[0]+"年"+sstr[1]+"月"+sstr[2]+"日"
+      alert(ans)
+      return ans
+  },
+  getDetails:function(){
+    var that=this
+    wx.request({
+      url: app.globalData.baseurl+'/reward/'+app.globalData.openid+'/getdetails',
+      method:'GET',
+      header:{
+        Authorization:wx.getStorageSync('token')
+      },
+      success:function(res){
+          that.setData({
+            details:res.data
+          })
+      }
+    })
+  },
+  change:function(){
+    var that=this
+    wx.request({
+      url: app.globalData.baseurl+'/reward/change',
+      data:{
+        openId:app.globalData.openid,
+        matter:"",
+        changed:"",
+        reward:this.data.soc-changed,
+      },
+      method:'POST',
+      header:{
+        Authorization:wx.getStorageSync('token')
+      },
+      success:function(res){
+        if(res.data.isOk){
+          wx.showToast({
+            title: '兑换成功',
+            icon: 'success',
+            duration: 2000,
+          })
+        }else if(res.data.isOk!==true){
+          wx.showToast({
+            title: '兑换失败',
+            duration: 2000
+          })
+        }
+      }
+    })
   },
 
   lumpadder:function(){
@@ -22,27 +95,8 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
-    //在加载时获取积分
-    /*
-    wx.request({
-      url: 'https://xcx.zxcwxy999.xyz/'+'订单管理',
-      method: 'POST',
-      //data:'',    //参数为键值对字符串
-      header: {
-        'content-type': 'application/json'
-      },
-      success: function (res) {
-        console.log(res.data)
-        that.setData({
-          soc: res.data
-        })
-      }
- 
-    })
-    */
-   
-
+    this.getlast()
+    this.getDetails()
   },
 
   /**
@@ -56,7 +110,8 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    this.getlast()
+    this.getDetails()
   },
 
   /**
